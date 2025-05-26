9bc028e9fc6765f032b37dc9bf2622b4ed7865ed
irarubrian <brian.o.iraru@gmail.com>
2025-05-26 23:20:11 +0300

    fouth commit

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,3 @@ from lib.app import create_app  # Imports your Flask app factory
 
 app = create_app()              # Initializes your Flask app
 
-if __name__ == "__main__":      # Runs the app with Flask's built-in server (for local dev)
-    app.run()
