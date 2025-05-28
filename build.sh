3f5fe81aeaf9e622acdf9169d4be531b71cb0f06
irarubrian <brian.o.iraru@gmail.com>
2025-05-28 13:10:41 +0300

    Fixed deployment configuration

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-set -o errexit
+set -ex  # Enable debugging
 
-# Install Pipenv
-pip install pipenv
+# 1. Ensure correct Python version
+python3.8 --version
 
-# Install dependencies system-wide
-pipenv install --system --deploy
+# 2. Install Pipenv explicitly
+python3.8 -m pip install --upgrade pip
+python3.8 -m pip install pipenv
 
-# Run migrations
-flask db upgrade
+# 3. System-wide install
+python3.8 -m pipenv install --system --deploy
+
+# 4. Verify installed packages
+pip freeze
+
+# 5. Run migrations
+python3.8 -m flask db upgrade
